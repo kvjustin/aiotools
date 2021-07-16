4646caa8b99fb85d1278b1f56b93b8fb269c8f66
Krishna <56835286+kvjustin@users.noreply.github.com>
2021-07-16 16:32:21 -0400

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+#------------------------------------------------------#
+#                     BOOM BABY                        #
+#                    Coded by KV                       #
+#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ #
+#     Instagram  :  instagram.com/the_solo._.rider     #
+#------------------------------------------------------#
+#               Dont try to Steal it Bitch             #
+#------------------------------------------------------#
+
+# Colors FG
+
+RED="$(printf '\e[31m')"
+GREEN="$(printf '\e[32m')"
+ORANGE="$(printf '\e[33m')"
+BLUE="$(printf '\e[34m')"
+MAGENTA="$(printf '\e[35m')"
+CYAN="$(printf '\e[36m')"
+WHITE="$(printf '\e[37m')"
+BLACK="$(printf '\e[30m')"
+
+# CODE STARTS FROM HERE
+
+clear
+echo "                                    $ORANGE Machine Working Progress Had Started"
+sleep 4
+echo "$CYAN"
+echo "Installing Required Tools For Installation"
+sleep 2
+echo "$GREEN"
+echo "Started Sequence"
+echo "$WHITE"
+apt-get install -y git curl wget pv espeak
+echo "$GREEN"
+echo "Sequence Compleated"
+sleep 2
+clear
+echo "$BLUE"
+echo "========================================================"
+echo "[+] Please Drink a Cup Of Sambar and Go To the Toilet..."
+echo "    Because this will take a long time."
+echo "========================================================"
+apt update
+apt install -y git
+apt install -y python
+apt install -y python2
+apt install -y wget
+apt install -y nmap
+apt install -y hydra
+apt update -y
+apt install -y git
+apt install python2
+cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git
+cd /data/data/com.termux/files/home
+apt install wget
+cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh
+cd /data/data/com.termux/files/home && bash metasploit.sh
+cd /data/data/com.termux/files/home && cd metasploit-framework
+cd /data/data/com.termux/files/home && gem install bundle --pre
+cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries
+cd /data/data/com.termux/files/home && bundle install
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y git
+cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y git
+apt install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git
+cd /data/data/com.termux/files/home && cd angryFuzzer
+cd /data/data/com.termux/files/home && pip2 install -r requirements.txt
+cd /data/data/com.termux/files/home && pip2 install requests
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y git
+apt install -y php 
+cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y python2
+apt install -y git
+cd /data/data/com.termux/files/home && git clone https://github.com/evait-security/weeman.git
+cd /data/data/com.termux/files/home && cd weeman
+cd /data/data/com.termux/files/home && chmod +x weeman.py
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y git
+apt install -y python
+cd /data/data/com.termux/files/home && git clone https://github.com/maldevel/IPGeoLocation.git
+cd /data/data/com.termux/files/home && cd IPGeoLocation
+cd /data/data/com.termux/files/home && pip install -r requirements.txt
+cd /data/data/com.termux/files/home
+apt update -y
+apt install -y git
+apt install -y python
+cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git
+apt update -y
+apt install -y git
+apt install -y python
+apt install -y nano
+pip install requests
+install beautifulsoup4
+cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git
+apt update -y
+apt install -y git
+apt install -y python
+pip install mechanicalsoup
+apt install -y nano
+cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git
+apt update -y
+apt install -y git
+apt install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git
+apt update -y
+apt install -y git
+apt install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git
+apt update -y
+apt install -y git
+apt install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/shawarkhanethicalhacker/D-TECT.git
+apt update -y
+apt install -y git
+apt install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git
+cd /data/data/com.termux/files/home && cd routersploit
+pip2 install -r requirements.txt
+pip2 install -r requirements-dev.txt
+pip2 install -r requests
+clear
+print("========================================")
+print("[+] everything successfully installed :)")
+print("[+] Happy Hacking <3")
+print("========================================")
